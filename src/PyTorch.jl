4a30f846c38a03d456d0cae2a4d1c0e9911bbbaa
Zenna Tavares <zennatavares@gmail.com>
2018-03-22 12:18:12 -0400

    add functional

--- src/PyTorch.jl
+++ src/PyTorch.jl
@@ -5,12 +5,20 @@ using PyCall
 @pyimport torch.nn as nn
 @pyimport torch.autograd as autograd
 @pyimport torch.optim as optim
+@pyimport torch.nn.functional as functional
 
 export nn,
        optim,
-       autograd
+       autograd,
+       functional
 
 "PyTorch Variable from `x`"
 variable(x::Array) = PyTorch.autograd.Variable(PyTorch.torch.Tensor(x))
-       
+
+# struct Tensor
+# end
+Tensor = PyObject
+
+export Tensor
+
 end
